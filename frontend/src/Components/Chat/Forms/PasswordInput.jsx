4baef6c71cb36bb57a70dde28c35ab8ea5f2ef60
Mac <mac@MacBook-Pro-Tim.local>
2022-11-10 01:09:31 +0300

    fix gug registration

--- frontend/src/Components/Chat/Forms/PasswordInput.jsx
+++ frontend/src/Components/Chat/Forms/PasswordInput.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { Form } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
 import { useField } from 'formik';
 
 const PasswordInput = ({
   label, authFailed, authFailedError, name,
 }) => {
-  const { t } = useTranslation();
   const [field, meta] = useField({ name });
 
   return (
@@ -28,7 +26,7 @@ const PasswordInput = ({
       </Form.Control.Feedback>
       {authFailedError && authFailed && (
         <Form.Control.Feedback type="invalid">
-          {t('forms.authFailed')}
+          {authFailedError}
         </Form.Control.Feedback>
       )}
     </Form.Group>

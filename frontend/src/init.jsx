cf5fc81a124836efb3337b2c5b0be17d90eb8bb7
Mac <mac@MacBook-Pro-Tim.local>
2022-11-11 02:35:45 +0300

    add chanels

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -68,6 +68,7 @@ const InitialState = async () => {
 
   socket.on('newChannel', (payload) => {
     store.dispatch(addChannel(payload));
+    store.dispatch(selectActiveChat(payload.id));
   });
 
   socket.on('newMessage', (payload) => {

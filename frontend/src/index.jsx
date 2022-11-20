1cb10346177b292afa94f670632ccc77152f0db9
Mac <mac@MacBook-Pro-Tim.local>
2022-11-21 02:35:56 +0300

    chat_bug

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import InitialState from './init';
+// eslint-disable-next-line import/no-relative-packages
+import { io } from '../node_modules/socket.io/client-dist/socket.io';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = async () => {
+  const socket = io();
   const root = ReactDOM.createRoot(document.getElementById('root'));
-  const startApp = await InitialState();
+  const startApp = await InitialState(socket);
   root.render(<React.StrictMode>{startApp}</React.StrictMode>);
 };
 

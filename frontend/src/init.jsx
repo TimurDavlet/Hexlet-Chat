6db5a1c831b279366ffe25864a1e989ee1fa9e80
Mac <mac@MacBook-Pro-Tim.local>
2022-11-21 02:42:28 +0300

    delete coment

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -64,7 +64,6 @@ const InitialState = async (socket) => {
 
   socket.on('newChannel', (payload) => {
     store.dispatch(addChannel(payload));
-    // store.dispatch(selectActiveChat(payload.id));
   });
 
   socket.on('newMessage', (payload) => {
